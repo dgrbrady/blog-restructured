b91497c83d5e1298edd49545ca64efd7f68eb5e2
David Brady <dgrbrady@gmail.com>
2018-04-05 15:55:49 -0400

    Added home page and user page

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return render_template("index.html", user_agent=user_agent)
+
+@app.route('/<user>')
+def user(user):
+    name = user
+    return render_template("user.html", name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
