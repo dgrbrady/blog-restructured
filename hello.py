f228b7dfabf8ca32f2eeeaa6d8bfa33cc0e75939
David Brady <dgrbrady@gmail.com>
2018-04-06 08:48:51 -0400

    Removed local time display and User-Agent from index.html but added web form

--- hello.py
+++ hello.py
@@ -2,15 +2,27 @@ from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'privateKeyString'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return render_template("index.html", user_agent=user_agent, current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("index.html", form=form, name=name)
 
 @app.route('/<user>')
 def user(user):
